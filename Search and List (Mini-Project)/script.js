7e16e9b91d8f3b3156ffcd66f29eba9ef6abeb11
RAVI KUMAR ISHWAR <159942657+RAVIKUMARISHWAR03@users.noreply.github.com>
2025-08-23 21:46:13 +0530

    Fix some errors

--- Search and List (Mini-Project)/script.js
+++ Search and List (Mini-Project)/script.js
@@ -1,66 +1,78 @@
-let userContainer = document.querySelector('.userContainer')
-let searchInput = document.querySelector('#searchInput');
-console.log(searchInput.value)
-
+let userContainer = document.querySelector(".userContainer")
+let searchInput = document.querySelector("#searchInput")
+console.log(searchInput.value);
 
 const users = [
-    {
-        profileUrl :'https://pbs.twimg.com/media/GRf52_kaYAAmZSS.jpg:large',
-        name :'Rohit Sharma',
-        email : 'rohitsharma45@.com',
-    },
-    {
-        profileUrl :'https://www.deccanchronicle.com/h-upload/2025/07/16/1938706-gv-dmoxkaaz9is.webp',
-        name :'Virat Kohli',
-        email : 'vk18@.com',
-    },
-    {
-        profileUrl :'https://assets.telegraphindia.com/telegraph/2024/Jan/1704461025_dhoni.jpg',
-        name :'Thala',
-        email : 'msdhoni07@.com',
-    },
-     {
-        profileUrl :'https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_800,q_50/lsci/db/PICTURES/CMS/21600/21640.jpg',
-        name :'Lala Amarnath',
-        email : 'lalaamarnath12@.com',
-    },
-     {
-        profileUrl :'https://static.cricbuzz.com/a/img/v1/i1/c209134/world-cup-winner-yashpal-sharma-passes-away-aged-66.jpg?d=low&p=det',
-        name :'Yashpal Sharma',
-        email : 'yashpalsharma68@.com',
-    },
-     {
-        profileUrl :'https://images.news18.com/ibnlive/uploads/2022/06/madan-lal-1.jpg',
-        name :'Madan Lal',
-        email : 'madanlal73@.com',
-    }
-   
-    
-
+  {
+    profileUrl: "https://pbs.twimg.com/media/GRf52_kaYAAmZSS.jpg:large",
+    name: "Rohit Sharma",
+    email: "rohitsharma45@.com",
+  },
+  {
+    profileUrl:
+      "https://www.deccanchronicle.com/h-upload/2025/07/16/1938706-gv-dmoxkaaz9is.webp",
+    name: "Virat Kohli",
+    email: "vk18@.com",
+  },
+  {
+    profileUrl:
+      "https://assets.telegraphindia.com/telegraph/2024/Jan/1704461025_dhoni.jpg",
+    name: "Thala",
+    email: "msdhoni07@.com",
+  },
+  {
+    profileUrl:
+      "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_800,q_50/lsci/db/PICTURES/CMS/21600/21640.jpg",
+    name: "Lala Amarnath",
+    email: "lalaamarnath12@.com",
+  },
+  {
+    profileUrl:
+      "https://static.cricbuzz.com/a/img/v1/i1/c209134/world-cup-winner-yashpal-sharma-passes-away-aged-66.jpg?d=low&p=det",
+    name: "Yashpal Sharma",
+    email: "yashpalsharma68@.com",
+  },
+  {
+    profileUrl:
+      "https://images.news18.com/ibnlive/uploads/2022/06/madan-lal-1.jpg",
+    name: "Madan Lal",
+    email: "madanlal73@.com",
+  },
 ]
-users.map(function(obj){
-  let {profileUrl, name,email} = obj 
-  console.log(profileUrl)
-  let divElem = document.createElement('div');
-  divElem.className = 'userItem'
-  divElem.innerHTML = `
-  <div class="image">
-                  <img src=${profileUrl} alt="error loading image">
-               </div>
-                <div class="userDetails">
-                    <h3>${name}</h3>
-                    <p>${email}</p>
-                </div>
-            </div> 
-    `
-    
-    
-    userContainer.append(divElem)
-}) 
-function handleSearch(e){
-   let searchValue = e.target.value
-   let filteredUsers = users.filter (obj =>{
-    obj.name
-   })
+
+function renderUsers(arr) {
+
+    userContainer.innerHTML = ''
+
+    arr.map(function (obj) {
+        let { profileUrl, name, email } = obj
+
+        let divElem = document.createElement('div');
+        divElem.className = 'userItem'
+        divElem.innerHTML = `
+            <div class="image">
+                <img src=${profileUrl} alt="error loading image">
+            </div>
+            <div class="userDetails">
+                <h3>${name}</h3>
+                <p>${email}</p>
+            </div>
+        `
+        userContainer.append(divElem);
+    })
 }
+
+renderUsers(users); // initial render
+
+function handleSearch(e) {
+    let searchValue = e.target.value;
+    let filteredUsers = users.filter(obj => {
+        return (
+            obj.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+            obj.email.toLowerCase().includes(searchValue.toLowerCase())
+        )
+    })
+    renderUsers(filteredUsers)
+}
+
 searchInput.addEventListener('input', handleSearch)
