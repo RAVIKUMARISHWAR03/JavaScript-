7707e662e8df7660baf750616edc781613a7f947
RAVI KUMAR ISHWAR <159942657+RAVIKUMARISHWAR03@users.noreply.github.com>
2025-08-31 12:47:17 +0530

    Add files via upload

---
+++ Todo-list/app.js
@@ -0,0 +1,42 @@
+let listContainer = document.querySelector('.listContainer');
+let addInput = document.querySelector('#addInput');
+let addBtn = document.querySelector('.addBtn');
+let list = [];
+
+
+function renderList(arr){
+listContainer.innerHTML=''
+arr.map(obj=>{
+  let divElem = document.createElement('div');
+  divElem.classList.add('list');
+  divElem.innerHTML= `
+   <p> ${obj.text}</p>
+  `
+  let buttonElem = document.createElement('button');
+  buttonElem.innerHTML ='❌';
+  divElem.appendChild(buttonElem)
+  buttonElem.onclick = ()=>{
+    handleRemoveList(obj.id)
+  } 
+  listContainer.append(divElem);
+})
+}
+renderList(list);
+function handleAddList(){
+    let obj ={
+        id:Date.now(),
+        text:addInput.value
+    }
+    list.unshift(obj);
+    renderList(list)
+     console.log(addInput.value)
+     addInput.value =' ';
+}
+function handleRemoveList(id){
+    let filteredList = list.filter(obj => {
+        return obj.id !== id
+    })
+    list = filteredList
+    renderList(list)
+} 
+addBtn.addEventListener('click',handleAddList)
