7fe599040fb156466a229d1d2460a5e11463428d
RAVI KUMAR ISHWAR <159942657+RAVIKUMARISHWAR03@users.noreply.github.com>
2026-01-09 07:28:14 +0530

    Create scripts.js

---
+++ AddFriend/scripts.js
@@ -0,0 +1,18 @@
+var istatus = document.querySelector("h5");
+
+var Btn = document.querySelector("#add");
+var check = 0;
+
+Btn.addEventListener("click", function () {
+  if (check == 0) {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+    Btn.innerHTML = "Remove Friend";
+    check = 1;
+  } else {
+    istatus.innerHTML = "Stranger";
+    istatus.style.color = "red";
+    Btn.innerHTML = "Add Friend";
+    check = 0;
+  }
+});
