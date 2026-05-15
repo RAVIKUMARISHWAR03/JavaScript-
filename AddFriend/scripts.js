a9eb237f42cd3c6b224266588c3dce8ee754905e
RAVI KUMAR ISHWAR <159942657+RAVIKUMARISHWAR03@users.noreply.github.com>
2026-05-15 23:41:04 +0530

    Implement friend status toggle on button click
    Change button functionality to toggle friend status.

--- AddFriend/scripts.js
+++ AddFriend/scripts.js
@@ -4,11 +4,13 @@ var Btn = document.querySelector("#add");
 var check = 0;
 
 Btn.addEventListener("click", function () {
+  
   if (check == 0) {
     istatus.innerHTML = "Friends";
     istatus.style.color = "green";
     Btn.innerHTML = "Remove Friend";
     check = 1;
+    
   } 
   else {
     istatus.innerHTML = "Stranger";
